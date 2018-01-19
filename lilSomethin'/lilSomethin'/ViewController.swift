31f4aaa2dc9b6689caf566e7614b0a2c1504c77c
kymwithay <hillkymberlee@gmail.com>
2018-01-19 18:23:59 -0500

    added an array of tip percentages and functionality code to control tip amount when updating tip percentage, amount is changed and total value is updated according to the percentage dynamically

--- lilSomethin'/lilSomethin'/ViewController.swift
+++ lilSomethin'/lilSomethin'/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    
-    @IBOutlet weak var billField: UITextField!
-    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,8 +30,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.05, 0.10, 0.15, 0.20]
+        
         let bill = Double(billField.text!) ?? 0
-        let tip = bill * 0.2
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
 
         tipLabel.text = String(format: "$%.2f", tip)
